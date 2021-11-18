81873661390c2348c43e4d3d228d47f96adb476c
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-11-17 23:30:31 -0300

    First commit

---
+++ src/test/java/com/matheucordeiro/financialapi/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheucordeiro.financialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
