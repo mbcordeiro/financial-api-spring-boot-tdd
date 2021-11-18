65e68baa36a5a71d23b80cb3df13b2eaeccd5270
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-11-18 00:00:34 -0300

    Profile entity and test

---
+++ src/test/java/com/matheucordeiro/financialapi/entities/ProfileTest.java
@@ -0,0 +1,25 @@
+package com.matheucordeiro.financialapi.entities;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ProfileTest {
+    @Test
+    public void shouldCreateBuildProfileObject() {
+        var profile = Profile.builder()
+                .id(1L)
+                .name("Matheus Cordeiro")
+                .email("matheusdebarroscordeiro@gmail.com")
+                .cpf("111111111111")
+                .phone("9999999999")
+                .build();
+
+        assertNotNull(profile);
+    }
+}
